62bef330e992b5a5f0e334a1fe0160a67f6fe58a
Stan <Stan@littleKali.com>
2015-08-03 11:20:48 +0200

    fixed stuff

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='v0lt',
-      version='1.0',
+      version='1.11',
       description='CTF oriented Toolkit',
       author='P1kachu',
       author_email='stanislas.lejay@epita.fr',

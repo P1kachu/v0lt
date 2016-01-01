bd3fe00c778c724a9582ca621a081cd37e42b462
P1kachu <stanislas.lejay@epita.fr>
2016-01-01 13:25:20 +0100

    Add verbose for generate_strings in bf

--- v0ltlib/tools/bruteforce.py
+++ v0ltlib/tools/bruteforce.py
@@ -11,38 +11,52 @@ class Bruteforce:
     final_length = 0
     begin_with = ""
     end_with = ""
+    max_iterations = 0
 
-    def __init__(self, charset, final_length, begin_with="", end_with=""):
+    def __init__(self, charset, final_length, begin_with="", end_with="", max_iterations=1):
         self.dictionnary = charset
         self.range_limit = len(self.dictionnary)
         self.begin_with = begin_with
         self.end_with = end_with + "\n"
         self.final_length = final_length
         self.length = self.final_length - (len(self.begin_with) + len(self.end_with) - 1)
+        self.max_iterations = max_iterations
 
-    def generate_strings(self, output=None):
+    def generate_strings(self, output=None, verbose=True):
         nb_of_lines = pow(len(self.dictionnary), self.length)
-
+        i = 0
         if output:
-            approx_size = sizeof_fmt((self.final_length + 1) * nb_of_lines, rounded=True)
-
-            warning("This may generate a very large file")
-            print("({0} permutations here == more than {1})".format(nb_of_lines, approx_size))
-
             f = open(output, "w")
-            debug("Bruteforcing...")
+            if verbose:
+                approx_size = sizeof_fmt((self.final_length + 1) * nb_of_lines, rounded=True)
+                warning("This may generate a very large file")
+                print("({0} permutations here == more than {1})".format(nb_of_lines, approx_size))
+                debug("Bruteforcing...")
+
             for n in range(self.length, self.length + 1):
+                if i > self.max_iterations and self.max_iterations > 0:
+                    break
                 for perm in itertools.product(self.dictionnary, repeat=n):
+                    i+=1
+                    if i > self.max_iterations and self.max_iterations > 0:
+                        break
                     bruted = ''.join(perm)
                     f.write(self.begin_with + bruted[::-1] + self.end_with)
             f.close()
 
-            success("File created ({0})".format(sizeof_fmt(os.path.getsize(output))))
+            success("Bruted file created ({0})".format(sizeof_fmt(os.path.getsize(output))))
 
         else:
-            warning("This may generate a very large output")
-            print("({0} permutations here)".format(nb_of_lines))
+            if verbose:
+                warning("This may generate a very large output")
+                print("({0} permutations here)".format(nb_of_lines))
+
             for n in range(self.length, self.length + 1):
                 for perm in itertools.product(self.dictionnary, repeat=n):
+                    if i > self.max_iterations and self.max_iterations > 0:
+                        break
+                    i+=1
                     bruted = ''.join(perm)
                     print(self.begin_with + bruted[::-1] + self.end_with, end="")
+                if i > self.max_iterations and self.max_iterations > 0:
+                    break

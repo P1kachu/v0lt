44080333159b466049fc2128cd3db6d2a9e09308
Stanislas P1kachu Lejay <p1kachu@lse.epita.fr>
2016-06-02 02:07:17 +0200

    Removed useless linux_utils
    Signed-off-by: Stanislas P1kachu Lejay <p1kachu@lse.epita.fr>

--- v0lt/examples.py
+++ v0lt/examples.py
@@ -50,14 +50,6 @@ class Tests(unittest.TestCase):
         bf = Bruteforce(charset="abcdef", final_length=12, begin_with="l", end_with="P")
         bf.generate_strings(output="bf.tmp")
 
-    def test_passwd_cracker(self):
-        nix_basic_pass_cracker("HX9LLTdc/jiDE")
-        nix_basic_pass_cracker("HX8LLTdc/jiDE")
-        # nix_basic_pass_cracker("$1$khkWa1Nz$7YcmdOO1/uyHhMB7ga2L.1")
-        # nix_basic_pass_cracker("$5$khkWa1Nz$583CsGZkoT82wh2ukf75KT4VVrf9ZO/P0FXLiPKgG//")
-        # nix_basic_pass_cracker("$6$P1$XKg/SKZpe8Gbl5Utt3XVJEA4zJ6KB.IuZlShnP2FljfF32z3zoytnB.MaP9dJOObSOtiidHmeBp.feOqK4Mvg/")
-
-
 if __name__ == "__main__":
     suite = unittest.defaultTestLoader.loadTestsFromTestCase(Tests)
     unittest.TextTestRunner().run(suite)
--- v0lt/toplevel.py
+++ v0lt/toplevel.py
@@ -6,7 +6,6 @@ from v0ltlib import *
 from v0ltlib.utils.stack import Stack
 from v0ltlib.utils.v0lt_utils import *
 from v0ltlib.utils.crypto_utils import *
-from v0ltlib.utils.linux_utils import *
 
 from v0ltlib.network.telnet import Telnet
 from v0ltlib.network.netcat import Netcat
